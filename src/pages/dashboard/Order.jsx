410b491d7237f07ecce01b043c98b922bfe91c2e
Subhash Prasad <96110162+subhashdippu@users.noreply.github.com>
2025-01-06 19:01:19 +0530

    Update Order.jsx

--- src/pages/dashboard/Order.jsx
+++ src/pages/dashboard/Order.jsx
@@ -12,7 +12,7 @@ const Order = () => {
     queryKey: ["orders", user?.email],
     queryFn: async () => {
       const res = await fetch(
-        `https://serverfoodapp.onrender.com/payments?email=${user?.email}`,
+        `https://backend-for-food-app.onrender.com/payments?email=${user?.email}`,
         {
           headers: {
             authorization: `Bearer ${token}`,
