49a26cf6239104ec39b1638c2dad79b27975f78a
Subhash Prasad <96110162+subhashdippu@users.noreply.github.com>
2025-01-06 18:58:48 +0530

    Update useAxiosPublic.jsx

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "https://serverfoodapp.onrender.com",
+    baseURL: "https://backend-for-food-app.onrender.com",
 });
 
 const useAxiosPublic = () => {
